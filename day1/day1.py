b733592c676f795fa304096ee75c05aa5a4f8ad2
BillyCWood <billyc.wood4@gmail.com>
2022-12-02 23:15:36 -0700

    new file:   day1/day1.py 	new file:   day1/day1a-input.txt 	deleted:    inputs/day1a-input.txt 	deleted:    solutions/day1.py

---
+++ day1/day1.py
@@ -0,0 +1,17 @@
+import sys
+
+def main():
+
+    highest_elf_num = 1 #1 because there is no elf 0
+    highest_calories = 0
+    current_elf_count = 1
+    current_count = 0
+
+
+    f = open(sys.argv[1], "r")
+
+    f.read()
+
+    f.close()
+
+main()
