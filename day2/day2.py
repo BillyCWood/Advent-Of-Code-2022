891034cc096a6710584e18d87ee471cbe62fe9fd
BillyCWood <billyc.wood4@gmail.com>
2022-12-04 23:15:51 -0700

    modified:   day2.py

--- day2/day2.py
+++ day2/day2.py
@@ -25,10 +25,25 @@ possibilities = {
     "C Z" : "draw"
 }
 
+part_two_possibilities = {
+    "A X" : "lost",
+    "A Y" : "draw",
+    "A Z" : "won",
+    "B X" : "lost",
+    "B Y" : "draw",
+    "B Z" : "won",
+    "C X" : "lost",
+    "C Y" : "draw",
+    "C Z" : "won"
+}
+
 shapes = {
     "X" : "rock",
     "Y" : "paper",
-    "Z" : "scissors"
+    "Z" : "scissors",
+    "A" : "rock",
+    "B" : "paper",
+    "C" : "scissors"
 }
 
 
@@ -37,19 +52,39 @@ def main():
 
 
     f = open("input2a.txt","r")
-
+    """
     for line in f:
         line = line.strip("\n")
         shape = line[2]
         score +=pointScoring[shapes[shape]]
         score += pointScoring[possibilities[line]]
+    """
+    for line in f:
+        line = line.strip("\n")
+        score+=pointScoring[part_two_possibilities[line]]
+        if(part_two_possibilities[line] == "draw"):
+            score+=pointScoring[shapes[line[0]]]
+        elif(part_two_possibilities[line] == "lost"):
+            if(line[0] == "A"):
+                score+=pointScoring["scissors"]
+            elif(line[0] == "B"):
+                score+=pointScoring["rock"]
+            else:
+                score+=pointScoring["paper"]
+            
+        else:
+            if(line[0] == "A"):
+                score+=pointScoring["paper"]
+            elif(line[0] == "B"):
+                score+=pointScoring["scissors"]
+            else:
+                score+=pointScoring["rock"]
+
+    f.close()
 
 
-
-    f.close
-
-
-    print("Part 1 - "+ str(score))
+    #print("Part 1 - "+ str(score))
+    print("Part 2 - " + str(score))
 
 
 main()
